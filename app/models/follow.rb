daz-codes/blood-sweat-and-beers

Extract model concerns following Fizzy composition pattern
Models now read as declarations of capabilities via focused concerns:

Shared concern:
- Notifiable — standardises after_create_commit notification pattern
  across WorkoutLike, Comment, and Follow

Model-namespaced concerns:
- Workout::Status — statuses, scopes, weight recording callback
- Workout::Likeable — most_liked_with_activity query + association
- Workout::StructureBuilder — JSON structure parsing from form params
- WorkoutLog::ExerciseLogBuilder — structure → exercise log creation
- Program::WorkoutBuilder — placeholder creation, build orchestration
- Program::Broadcasting — Turbo stream broadcasts
- User::Billing — pro?/free? plan predicates
- User::FitnessTracking — benchmark tracking + association
- User::ExerciseWeightRecorder — weight extraction from structures

Co-Authored-By: Claude Opus 4.6 <noreply@anthropic.com>

app/models/follow.rb
@@ -1,4 +1,6 @@
 class Follow < ApplicationRecord
+  include Notifiable
+
   belongs_to :follower,  class_name: "User"
   belongs_to :following, class_name: "User"
 
@@ -7,8 +9,9 @@ class Follow < ApplicationRecord
 
   before_create { self.requested_at = Time.current }
 
-  after_create_commit  :notify_follow_request
-  after_update_commit  :notify_follow_accepted, if: -> { saved_change_to_status?(from: "pending", to: "accepted") }
+  notifies action: :follow_request, recipient: :following, actor: :follower
+
+  after_update_commit :notify_follow_accepted, if: -> { saved_change_to_status?(from: "pending", to: "accepted") }
 
   scope :pending,  -> { where(status: "pending") }
   scope :accepted, -> { where(status: "accepted") }
@@ -23,21 +26,12 @@ class Follow < ApplicationRecord
     errors.add(:follower_id, "can't follow yourself") if follower_id == following_id
   end
 
-  def notify_follow_request
-    Notification.create!(
-      recipient: following,
-      actor:     follower,
-      notifiable: self,
-      action:    "follow_request"
-    )
-  end
-
   def notify_follow_accepted
     Notification.create!(
-      recipient: follower,
-      actor:     following,
+      recipient:  follower,
+      actor:      following,
       notifiable: self,
-      action:    "follow_accepted"
+      action:     "follow_accepted"
     )
   end
 end

daz-codes/volt

app/models/follow.rb
@@ -1,4 +1,6 @@
 class Follow < ApplicationRecord
+  include Notifiable
+
   belongs_to :follower,  class_name: "User"
   belongs_to :following, class_name: "User"
 
@@ -11,7 +13,7 @@ class Follow < ApplicationRecord
     self.accepted_at  = Time.current
   end
 
-  after_create_commit :notify_new_follower
+  notifies action: :new_follower, recipient: :following, actor: :follower
 
   scope :accepted, -> { where(status: "accepted") }
 
@@ -19,14 +21,5 @@ class Follow < ApplicationRecord
 
   def cannot_follow_self
     errors.add(:follower_id, "can't follow yourself") if follower_id == following_id
-  end
-
-  def notify_new_follower
-    Notification.create!(
-      recipient:  following,
-      actor:      follower,
-      notifiable: self,
-      action:     "new_follower"
-    )
   end
 end