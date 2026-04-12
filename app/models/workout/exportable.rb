81929050c91c1a6113c25b2b18162c35c252dd3e
DAZ <darren.jones@hey.com>
2026-04-12 20:30:36 +0100

    small change to wording on images

--- app/models/workout/exportable.rb
+++ app/models/workout/exportable.rb
@@ -223,8 +223,8 @@ module Workout::Exportable
       rotating = style == "rotating" || (style.blank? && ex_count > 1)
       if rotating
         cycles = dur && ex_count > 0 ? dur / ex_count : nil
-        desc = cycles && cycles > 1 ? "#{dur}min \u00B7 #{cycles} rounds" : "#{dur}min"
-        [ "Circuit", desc ]
+        desc = cycles && cycles > 1 ? "1 min each exercise, #{cycles} rounds" : "1 min each exercise"
+        [ "Continuous Circuit", desc ]
       elsif ex_count >= 3 && dur
         e2m_rounds = dur / 2
         desc = e2m_rounds > 1 ? "All #{ex_count} exercises every 2 min \u00B7 #{e2m_rounds} rounds" : nil
