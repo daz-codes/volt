26b915110965587a23f1979ba67ecaf145beb513
DAZ <darren.jones@hey.com>
2026-04-22 07:37:56 +0100

    test(activity): bump DEFAULT_NAMES count to 18 and assert Turbine inclusion

--- test/models/activity_test.rb
+++ test/models/activity_test.rb
@@ -31,7 +31,8 @@ class ActivityTest < ActiveSupport::TestCase
     assert_includes Activity::DEFAULT_NAMES, "Iron Engine"
     assert_includes Activity::DEFAULT_NAMES, "Sunday Workout"
     assert_includes Activity::DEFAULT_NAMES, "Volt Octathlon"
-    assert_equal 17, Activity::DEFAULT_NAMES.size
+    assert_includes Activity::DEFAULT_NAMES, "Turbine"
+    assert_equal 18, Activity::DEFAULT_NAMES.size
   end
 
   test "defaults scope returns only default activities" do
